4b47ce6d889f4aef6f457fbd468ce6fbe10bec62
sgm4247 <sgm4247@naver.com>
2023-09-14 21:29:48 +0900

    망해따

--- src/pages/board/components/onePost.js
+++ src/pages/board/components/onePost.js
@@ -1,20 +1,31 @@
 import { styled } from "styled-components";
 import { useState } from "react";
+
+import OneComment from "./comments/onecomment";
+import AddComments from "./comments/addComments";
+// import { usePost } from "../../../boardContext";
 import AddPostFix from "./addPostFix";
 import AddPostDel from "./addPostDel";
 
 const OnePost = ({ post }) => {
   // 이미지 페이지 네이션
   const [imgIndex, setImgIndex] = useState(0);
-  const beforeImg = () => {
+
+  // 댓글 보기
+  const [isComments, setIsComments] = useState(false);
+
+  const onBeforeImg = () => {
     imgIndex === 0
       ? setImgIndex(post.Post_img.length - 1)
       : setImgIndex(imgIndex - 1);
   };
-  const nextImg = () => {
+  const onNextImg = () => {
     imgIndex === post.Post_img.length - 1
       ? setImgIndex(0)
       : setImgIndex(imgIndex + 1);
+  };
+  const onShowComments = () => {
+    setIsComments((prev) => !prev);
   };
 
   return (
@@ -26,20 +37,45 @@ const OnePost = ({ post }) => {
           {post.User.nickName}
           {/* 게시물 기능버튼 추가 */}
           {post.myPost && (
-            <div>
+            <PostBtn>
               <AddPostFix postid={post.id} />
               <AddPostDel postid={post.id} />
-            </div>
+            </PostBtn>
           )}
         </PostUser>
         <PostTitle>{post.title}</PostTitle>
         <ContentImgWrapper>
-          {post.Post_img.length > 1 && <button onClick={beforeImg}>◀</button>}
+          {post.Post_img.length > 1 && <button onClick={onBeforeImg}>◀</button>}
           <ContentImg src={post.Post_img[imgIndex]} alt="img" />
-          {post.Post_img.length > 1 && <button onClick={nextImg}>▶</button>}
+          {post.Post_img.length > 1 && <button onClick={onNextImg}>▶</button>}
         </ContentImgWrapper>
+
         <PostContent>{post.content}</PostContent>
-        <button>댓글 보기</button> {/* 여기에 댓글 로직 추가해주세요 */}
+        <button onClick={onShowComments}>
+          {isComments ? "댓글닫기" : "댓글보기"}
+        </button>
+        {isComments && (
+          <div>
+            {post.Comments.map((comment) => (
+              <>
+                <CommentBoxWrapper>
+                  <ProfileImg
+                    src={post.User.profileImg}
+                    alt="img"
+                    style={{ padding: "10px" }}
+                  />
+                  <OneComment
+                    comments={comment}
+                    posts={post}
+                    postId={post.id}
+                    commentsId={comment.id}
+                  />
+                </CommentBoxWrapper>
+              </>
+            ))}
+            <AddComments postId={post.id} />
+          </div>
+        )}
       </PostBox>
     </PostPage>
   );
@@ -55,7 +91,7 @@ const PostPage = styled.div`
 const PostBox = styled.div`
   border: 1px solid black;
   width: 1000px;
-  height: 540px;
+  height: auto;
   margin-bottom: 32px;
   margin-top: 32px;
 `;
@@ -81,8 +117,8 @@ const ContentImgWrapper = styled.div`
   border: 1px solid black;
   height: 400px;
   display: flex;
-  justify-content: center; /* 이미지를 수평으로 가운데 정렬합니다 */
-  align-items: center; /* 이미지를 수직으로 가운데 정렬합니다 */
+  justify-content: center;
+  align-items: center;
 `;
 const ContentImg = styled.img`
   width: 400px;
@@ -91,3 +127,15 @@ const ContentImg = styled.img`
 
 // 내용
 const PostContent = styled.div``;
+
+//댓글
+
+const CommentBoxWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+//
+const PostBtn = styled.div`
+  display: flex;
+  justify-content: end;
+`;
